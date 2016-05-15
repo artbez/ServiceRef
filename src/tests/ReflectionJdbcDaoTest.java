01f937977189946969e20fa0a281de007e104f21
Artemii Bezguzikov <artemii.bezguzikov@gmail.com>
2016-05-15 03:36:39 +0300

    add unit tests for all methods for current version

--- src/tests/ReflectionJdbcDaoTest.java
+++ src/tests/ReflectionJdbcDaoTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.Test;
 
 import DOMClasses.ExampleDOMTwo;
@@ -22,31 +23,145 @@ public class ReflectionJdbcDaoTest {
 		Query query = session.createQuery("delete ExampleDOMTwo where objId = '-1'");
 		query.executeUpdate();
 		session.getTransaction().commit();
-		session.getTransaction().begin();
-		query = session.createQuery("delete ExampleDOMTwo where objId = '-2'");
-		query.executeUpdate();
-		session.getTransaction().commit();
+		
 		session.getTransaction().begin();
 		ReflectionJdbcDaoImpl<ExampleDOMTwo> myRefService = new ReflectionJdbcDaoImpl<>();
 		ExampleDOMTwo myEx1 = new ExampleDOMTwo(new Long(-1));
 		myEx1.setName("test1");
-		ExampleDOMTwo myEx2 = new ExampleDOMTwo(new Long(-2));
-		myEx2.setName("test2");
+		
 		myRefService.insert(myEx1);
-		myRefService.insert(myEx2);
 		
 		query = session.createQuery("from ExampleDOMTwo where objId = '-1'");
 		List<ExampleDOMTwo> list = query.list();
 		assertEquals(myEx1.getObjId(), list.get(0).getObjId());
 		assertEquals(myEx1.getName(), list.get(0).getName());
-		query = session.createQuery("from ExampleDOMTwo where objId = '-2'");
-		list = query.list();
-		assertEquals(myEx2.getObjId(), list.get(0).getObjId());
-		assertEquals(myEx2.getName(), list.get(0).getName());
 		
 		session.getTransaction().commit();
 	    session.close();
+	}
+	
+	@Test
+	public void updateTest(){
 		
+		Session session = HibernateSessionFactory.getSessionFactory().openSession(); 
+		session.beginTransaction();
+		Query query = session.createQuery("delete ExampleDOMTwo where objId = '-1'");
+		query.executeUpdate();
+		session.getTransaction().commit();
+		
+		ReflectionJdbcDaoImpl<ExampleDOMTwo> myRefService = new ReflectionJdbcDaoImpl<>();
+		ExampleDOMTwo myEx1 = new ExampleDOMTwo(new Long(-1));
+		myEx1.setName("test1");
+
+		myRefService.insert(myEx1);
+		
+		myEx1.setName("updatedTest1");
+		myRefService.update(myEx1);
+		
+		query = session.createQuery("from ExampleDOMTwo where objId = '-1'");
+		List<ExampleDOMTwo> list = query.list();
+		assertEquals(myEx1.getName(), list.get(0).getName());
+		session.close();
+	}
+	
+	@Test
+	public void deleteByKeyTest(){
+		
+		Session session = HibernateSessionFactory.getSessionFactory().openSession(); 
+		session.beginTransaction();
+		Query query = session.createQuery("delete ExampleDOMTwo where objId = '-1'");
+		query.executeUpdate();
+		session.getTransaction().commit();
+		
+		ReflectionJdbcDaoImpl<ExampleDOMTwo> myRefService = new ReflectionJdbcDaoImpl<>();
+		ExampleDOMTwo myEx1 = new ExampleDOMTwo(new Long(-1));
+		myEx1.setName("testDelete");
+		session.beginTransaction();
+		query = session.createQuery("from  ExampleDOMTwo where objId = '-1'");
+		session.getTransaction().commit();
+		boolean exists = query.uniqueResult() != null;
+		assertFalse(exists);
+		
+		myRefService.insert(myEx1);
+		session.beginTransaction();
+		query = session.createQuery("from  ExampleDOMTwo where objId = '-1'");
+		session.getTransaction().commit();
+		exists = query.uniqueResult() != null;
+		
+		assertTrue(exists);
+		
+		myRefService.deleteByKey(myEx1);
+		
+		session.beginTransaction();
+		query = session.createQuery("from  ExampleDOMTwo where objId = '-1'");
+		session.getTransaction().commit();
+		exists = query.uniqueResult() != null;
+		
+		assertFalse(exists);
+		session.close();
+	}
+	
+	@Test
+	public void selectByKeyTest() {
+		
+		Session session = HibernateSessionFactory.getSessionFactory().openSession(); 
+		session.beginTransaction();
+		Query query = session.createQuery("delete ExampleDOMTwo where objId = '-1'");
+		query.executeUpdate();
+		session.getTransaction().commit();
+		
+		ReflectionJdbcDaoImpl<ExampleDOMTwo> myRefService = new ReflectionJdbcDaoImpl<>();
+		ExampleDOMTwo myEx1 = new ExampleDOMTwo(new Long(-1));
+		myEx1.setName("test1");
+
+		myRefService.insert(myEx1);
+		
+		ExampleDOMTwo myEx2 = new ExampleDOMTwo(new Long(-1));
+		myEx2 = myRefService.selectByKey(myEx2);
+		
+		assertEquals(myEx1.name, myEx2.name);
+		session.close();
+	}
+
+	@Test
+	public void selectAllTest(){
+
+		Session session = HibernateSessionFactory.getSessionFactory().openSession(); 
+		session.beginTransaction();
+		Query query = session.createQuery("delete ExampleDOMTwo where objId = '-1'");
+		query.executeUpdate();
+		session.getTransaction().commit();
+		
+		ReflectionJdbcDaoImpl<ExampleDOMTwo> myRefService = new ReflectionJdbcDaoImpl<>();
+		ExampleDOMTwo myEx1 = new ExampleDOMTwo(new Long(-1));
+		myEx1.setName("test1");
+
+		myRefService.insert(myEx1);
+		
+		session.beginTransaction();
+		query = session.createQuery("delete ExampleDOMTwo where objId = '-2'");
+		query.executeUpdate();
+		session.getTransaction().commit();
+		
+		ExampleDOMTwo myEx2 = new ExampleDOMTwo(new Long(-2));
+		myEx1.setName("test2");
+
+		try {
+			List<ExampleDOMTwo> mList = myRefService.selectAll(Class.forName("DOMClasses.ExampleDOMTwo"));
+			Transaction tx = session.beginTransaction();
+			query = session.createQuery("from ExampleDOMTwo");
+			List<ExampleDOMTwo> expectedList = query.list();
+			tx.commit();
+			boolean allEquals = mList.size() == expectedList.size();
+			for (int i = 0; i < mList.size(); ++i) 
+				if (mList.get(i).equals(expectedList.get(i)))
+					allEquals = false;
+			assertTrue(allEquals);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		session.close();
 	}
 
 }
